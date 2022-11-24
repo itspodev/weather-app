db05d9e2267d673c4cde66e8a7f706d276a1bd46
P-O Mpolesha <po.mpolesha@gmail.com>
2022-11-24 19:50:46 +0100

    SCSS

--- src/components/LocationInput.jsx
+++ src/components/LocationInput.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function LocationInput({ location, onChangeLocation }) {
   return (
     <>
-      <label htmlFor="location"></label>
-      <input
-        type="text"
-        value={location}
-        onChange={(event) => onChangeLocation(event.target.value)}
-        placeholder="Rechercher une ville"
-      ></input>
+      <div className="search-weather-location">
+        <label htmlFor="location"></label>
+        <input
+          type="text"
+          value={location}
+          onChange={(event) => onChangeLocation(event.target.value)}
+          placeholder="Rechercher une ville"
+        ></input>
+        <button type="submit" onSubmit={(e) => e.preventDefault}>
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </button>
+      </div>
     </>
   );
 }
